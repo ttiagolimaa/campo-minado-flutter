ff5ebe11f2b3ac36a3a02f7780e30f75a383ab6c
Tiago Lima <tiagocolima@gmail.com>
2021-06-10 13:11:10 -0300

    widget campo

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,3 +1,6 @@
+import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/explosao_exeption.dart';
+import 'package:campo_minado/widgets/campo_widget.dart';
 import 'package:campo_minado/widgets/resultado_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,18 +9,40 @@ class CampoMinadoApp extends StatelessWidget {
     print('reiniciar...');
   }
 
+  _abrir(Campo campo) {
+    print('abir....');
+  }
+
+  _alternarMarcacao(Campo campo) {
+    print('alternar...');
+  }
+
   @override
   Widget build(BuildContext context) {
+    Campo vizinho = Campo(linha: 0, coluna: 1);
+    Campo vizinho1 = Campo(linha: 1, coluna: 1);
+    Campo campo = Campo(coluna: 0, linha: 0);
+    campo.alternarMarcacao();
+    // campo.adicionarVizinho(vizinho);
+    // campo.adicionarVizinho(vizinho1);
+    // vizinho.minar();
+    // vizinho1.minar();
+
+    try {
+      // campo.abrir();
+    } on ExplosaoExeption {}
+
     return MaterialApp(
       home: Scaffold(
-        appBar: ResultadoWidget(
-          onReiniciar: _reiniciar,
-          venceu: null,
-        ),
-        body: Center(
-          child: Text('Tabuleiro'),
-        ),
-      ),
+          appBar: ResultadoWidget(
+            onReiniciar: _reiniciar,
+            venceu: null,
+          ),
+          body: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          )),
     );
   }
 }
---
+++ lib/widgets/campo_widget.dart
@@ -0,0 +1,38 @@
+import 'package:campo_minado/models/campo.dart';
+import 'package:flutter/material.dart';
+
+class CampoWidget extends StatelessWidget {
+  final Campo campo;
+  final void Function(Campo) onAbrir;
+  final void Function(Campo) onAlternarMarcacao;
+
+  const CampoWidget({
+    required this.campo,
+    required this.onAbrir,
+    required this.onAlternarMarcacao,
+  });
+
+  Widget _getImage() {
+    if (campo.explodido) return Image.asset('assets/images/bomba_0.jpeg');
+
+    if (campo.aberto && campo.minado)
+      return Image.asset('assets/images/bomba_1.jpeg');
+
+    if (campo.aberto)
+      return Image.asset(
+          'assets/images/aberto_${campo.qtdeMinasNaVizinhanca}.jpeg');
+
+    if (campo.marcado) return Image.asset('assets/images/bandeira.jpeg');
+
+    return Image.asset('assets/images/fechado.jpeg');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onAbrir(campo),
+      onLongPress: () => onAlternarMarcacao(campo),
+      child: _getImage(),
+    );
+  }
+}
