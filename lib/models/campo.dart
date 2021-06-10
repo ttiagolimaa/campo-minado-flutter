72d209c6891547908c41e8d524256fa6729476c8
Tiago Lima <tiagocolima@gmail.com>
2021-06-10 09:04:48 -0300

    create model campo

---
+++ lib/models/campo.dart
@@ -0,0 +1,80 @@
+import 'explosao_exeption.dart';
+
+class Campo {
+  final int linha;
+  final int coluna;
+  final List<Campo> vizinhos = [];
+
+  bool _aberto = false;
+  bool _marcado = false;
+  bool _minado = false;
+  bool _explodido = false;
+
+  Campo({
+    required this.linha,
+    required this.coluna,
+  });
+
+  bool get minado => _minado;
+  bool get explodido => _explodido;
+  bool get aberto => _aberto;
+  bool get marcado => _marcado;
+
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha = (linha - vizinho.linha).abs();
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) return;
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (_aberto) return;
+
+    _aberto = true;
+
+    if (_minado) {
+      _explodido = true;
+      throw ExplosaoExeption();
+    }
+
+    if (vizinhacaSegura) vizinhos.forEach((element) => element.abrir());
+  }
+
+  bool get vizinhacaSegura {
+    return vizinhos.every((element) => !element.minado);
+  }
+
+  void revelarBomba() {
+    if (_minado) _aberto = true;
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
+
+  bool get resolvido {
+    bool minadoEMarcado = minado && marcado;
+    bool seguroEAberto = !minado && aberto;
+
+    return minadoEMarcado || seguroEAberto;
+  }
+
+  int get qtdeMinasNaVizinhanca {
+    return vizinhos.where((v) => v.minado).length;
+  }
+}
